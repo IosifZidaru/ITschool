05be4928e0b42762b3e39e9f8c93ba32ee5a4859
iosif <iosifadelin302gmail.com>
2025-12-21 15:16:27 +0000

    season10

---
+++ src/session10/TipuriMostenire.java
@@ -0,0 +1,34 @@
+package session10;
+
+public class TipuriMostenire {
+}
+
+class A{}
+
+class B extends A{}
+
+
+class Buildind{}
+
+class Apartament extends Buildind{}
+
+class Room extends Apartament{}
+
+
+class Supercar{}
+
+class Ferrari extends Supercar{}
+
+class Lamborghini extends Supercar{}
+
+
+class Shape{}
+
+class Squere extends Shape{}
+class Dreptunghi extends Shape{}
+
+
+
+
+
+
